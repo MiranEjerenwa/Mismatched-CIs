8bbe0e66a9b932de1f0f36b6530686fee1cb2509
Miotti <210061319@LOGON.DS.GE.COM>
2019-03-16 23:11:55 -0400

    Cidata model fix and adding SQL data seed files

---
+++ sqlfiles/departments_seeds.sql
@@ -0,0 +1,13 @@
+-- Load this file in SQL Workbench, modify the DB name in the USE statement, 
+-- if necessary, and execute the Insert statements to populate seed data. 
+-- Load order is department_seeds, users_seeds, Cidata_seeds
+USE mismatchedci_dev_db; 
+INSERT INTO Departments (department, createdAt, updatedAt) VALUES
+('Digital Risk Staff', NOW(), NOW()),
+('Digital Technology', NOW(), NOW()),
+('Digital Support', NOW(), NOW()),
+('Digital Operations', NOW(), NOW()),
+('DIG Engineering', NOW(), NOW()),
+('Digital Quality', NOW(), NOW());
+
+--adding
