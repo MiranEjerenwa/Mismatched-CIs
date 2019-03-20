44921541862e527a2427eefa514c4ed81c1af6f3
Mirian <mirianejerenwa@yahoo.com>
2019-03-20 13:45:15 -0400

    code cleanup

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,31 +10,31 @@ const db = require('../models');
 //restAPI
 module.exports = function (app) {
 
-  const mismatched_ci = new RestfulAPI('Mismatched_CI', app, models.Cidata);
+const mismatched_ci = new RestfulAPI('Mismatched_CI', app, models.Cidata);
   mismatched_ci.findAll();
   mismatched_ci.find('id');
   mismatched_ci.create();
   mismatched_ci.delete('id');
   mismatched_ci.update('id');
 
-  // API for login
+// API for login
 
-app.post('/api/login', function(req,res) {
-  if(userVerification(req.body.username,req.body.password)){
-    var token = jwt.sign({ id: req.body.username}, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-    });
-    currentUser = req.body.username;
-    console.log(`The token is ${token}`);
-    res.cookie('token',token).json({auth:true,redirect:'dashboard'});
+  app.post('/api/login', function(req,res) {
+    if(userVerification(req.body.username,req.body.password)){
+      var token = jwt.sign({ id: req.body.username}, config.secret, {
+        expiresIn: 86400 // expires in 24 hours
+          });
+      currentUser = req.body.username;
+      console.log(`The token is ${token}`);
+      res.cookie('token',token).json({auth:true,redirect:'dashboard'});
     
-  }else{
-    res.status(401).send("You are not authorized");
-  }
+    }else{
+      res.status(401).send("You are not authorized");
+        }
 
 });
 
-      // POST route for saving a new user
+  // POST route for saving a new user
   app.post('/api/user', function(req, res) {
     db.User.create(req.body).then(function(dbuser) {
       res.json(dbuser);
@@ -51,41 +51,5 @@ app.post('/api/login', function(req,res) {
       res.json({ error: error });
     });
   });
-
-
-  // app.get('/api/user', function (req, res) {
-  //   db.users.findAll({}).then(function (rows) {
-  //     // sending back to the user data to the front end
-  //     res.json(rows);
-  //     console.log(req.method + " request");
-  //     console.log(JSON.stringify(rows))
-  //   }).catch(function (error) {
-  //     res.json({ error: error });
-  //   });
-  // });
-
-  
-// // Popping in the middle ware
-// app.use(function(req, res, next){
-//   //Add an express-validator check to ensure the field isn't empty and it contains a valid JWT
-//   var token = req.headers.authorization;
-//   if(token){
-//     jwt.verify(token, config.secret, function(err, decoded) {
-//       if(err || (decoded.id != currentUser)){
-//         res.status(403).json({
-//           auth: false,
-//           message:"Incorrect or missing token"
-//         });
-//       }else{
-//         next();
-//       }    
-//     });
-//   }else
-//   res.status(403).json({
-//     auth: false,
-//     message:"Incorrect or missing token"
-//   });  
-// });
-
 
 }
