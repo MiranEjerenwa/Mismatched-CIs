b4f6ab2d6b92e818ca030e611b11b48a8bcfbaa0
Mirian <mirianejerenwa@yahoo.com>
2019-03-19 21:26:56 -0400

    pieWorkingPartial

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -35,22 +35,22 @@ app.post('/api/login', function(req,res) {
 });
 
       // POST route for saving a new user
-  app.post('/api/user', function(req, res) {
-    db.user.create(req.body).then(function(dbuser) {
-      res.json(dbuser);
-    }).catch(function(error) {
-      res.json({ error: error });
-    });
-  });
+  // app.post('/api/user', function(req, res) {
+  //   db.user.create(req.body).then(function(dbuser) {
+  //     res.json(dbuser);
+  //   }).catch(function(error) {
+  //     res.json({ error: error });
+  //   });
+  // });
 
-  // GET route for seeing all users
-  app.get('/api/user', function(req, res) {
-    db.user.create(req.body).then(function(dbuser) {
-      res.json(dbuser);
-    }).catch(function(error) {
-      res.json({ error: error });
-    });
-  });
+  // // GET route for seeing all users
+  // app.get('/api/user', function(req, res) {
+  //   db.user.create(req.body).then(function(dbuser) {
+  //     res.json(dbuser);
+  //   }).catch(function(error) {
+  //     res.json({ error: error });
+  //   });
+  // });
   
 // // Popping in the middle ware
 // app.use(function(req, res, next){
